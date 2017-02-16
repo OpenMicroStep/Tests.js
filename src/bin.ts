1a53a8ae39e4777244f7efe1ea9b4a497d5a21bf
Vincent Rouillé <v-rouille@logitud.fr>
2017-02-16 10:54:52 +0100

    fix: catch unhandled promise rejection

--- src/bin.ts
+++ src/bin.ts
@@ -5,6 +5,7 @@ import {run} from './tests';
 let Module = require('module');
 let req = Module.prototype.require;
 let test_files = process.argv.filter(a => a.endsWith('.js'));
+process.on('unhandledRejection', (reason, promise) => { throw reason });
 Module.prototype.require = function(this: any, path: string) {
     if (test_files.findIndex(f => path.endsWith(f)) !== -1) {
         let m = req.apply(this, arguments);
