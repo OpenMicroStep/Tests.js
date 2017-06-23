b4d2ed7d4610c8860d199cc0444b2bdc640ca09c
Vincent Rouillé <v-rouille@logitud.fr>
2017-06-23 15:51:05 +0200

    fix tests on windows

--- src/bin.ts
+++ src/bin.ts
@@ -2,12 +2,23 @@
 
 import {Async} from '@openmicrostep/async';
 import {run} from './tests';
+import * as path from 'path';
+
+export function pathNormalized(p: string) {
+  p = path.normalize(p).replace(/\\/g, '/');
+  if (p.endsWith('/'))
+    p = p.substring(0, p.length - 1);
+  return p;
+}
+
+
 let Module = require('module');
 let req = Module.prototype.require;
-let test_files = process.argv.filter(a => a.endsWith('.js'));
+let test_files = process.argv.filter(a => a.endsWith('.js')).map(a => pathNormalized(a));
 process.on('unhandledRejection', (reason, promise) => { throw reason });
-Module.prototype.require = function(this: any, path: string) {
-    if (test_files.findIndex(f => path.endsWith(f)) !== -1) {
+Module.prototype.require = function(this: any, modulePath: string) {
+    modulePath = pathNormalized(modulePath);
+    if (test_files.findIndex(f => modulePath.endsWith(f)) !== -1) {
         let m = req.apply(this, arguments);
         Async.run(null, (f) => run(f, m));
     }
--- tst/test.js
+++ tst/test.js
@@ -2,6 +2,7 @@
 module.exports = { name: "my module", tests: [
     { name: "my submodule", tests: [
         function my_test() {},
-        { name: "my_2nd_test", test: function my_2nd_test() {} }
+        { name: "my_2nd_test", test: function my_2nd_test() {} },
+        function my_async_test(flux) { flux.continue() },
     ]}
 ]};
